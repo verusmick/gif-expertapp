43e85781f06e1cd64814065e9c36f65bdcd7a7ea
Micky <miguel.angel.angulo72@gmail.com>
2020-09-09 15:40:02 -0400

    Added docs

---
+++ docs/precache-manifest.0f0722be267ac47524a91b52b55317d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144e7a586283ab2bffa500e745795e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8471ebbfa25746c866b3",
+    "url": "/static/css/main.7242ab8f.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8471ebbfa25746c866b3",
+    "url": "/static/js/main.e5bee87b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
